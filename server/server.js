8a90ac0b2840bebe4b84ab8632ad72576016f820
Sergio Cruz <hello@sergiocruz.me>
2016-11-10 14:42:12 -0500

    Simplified architecture for deployment

--- server/server.js
+++ server/server.js
@@ -16,28 +16,40 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////////////////
 
+const path = require('path');
 const express = require('express');
 const app = express();
-
 const request = require('request');
-const credentials = (require ('fs').existsSync ('./credentials.js') ?
-    require ('./credentials')
-    : (console.log ('No credentials.js file present, assuming using CONSUMERKEY & CONSUMERSECRET system variables.'), 
-    require ('./credentials_'))) ;
+const buildDirectory = path.join(__dirname, '../build')
+const credentials = (
+  require ('fs').existsSync(path.join(__dirname, 'credentials.js'))
+    ? require ('./credentials')
+    : (
+        console.log('No credentials.js file present, assuming using CONSUMERKEY & CONSUMERSECRET system variables.'),
+        require ('./credentials_')
+      )
+);
 
 
 app.set('port', (process.env.API_PORT || 3001));
+app.use(express.static(buildDirectory))
+
+app.get('/', function (req, res) {
+  res.sendFile(`${buildDirectory}/index.html`)
+})
 
 app.get ('/token', function (req, res) {
-    request.post (
-        credentials.Authentication,
-        { form: credentials.credentials },
-        function (error, response, body) {
-            if ( !error && response.statusCode == 200 )
-                res.send (body) ;
-        }) ;
-}) ;
+  request.post(
+    credentials.Authentication,
+    { form: credentials.credentials },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.json(JSON.parse(body)) ;
+      }
+    });
+});
 
 app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+  // eslint-disable-line no-console
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
 });
