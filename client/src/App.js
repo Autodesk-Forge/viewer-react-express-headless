458bed4be86d98f25281d113bd58eb543787c231
Sergio Cruz <hello@sergiocruz.me>
2016-11-10 12:17:24 -0500

    Running local boostrap

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Jumbotron from './Jumbotron';
 import Footer from './Footer';
 import Gallery from './Gallery';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import 'font-awesome/css/font-awesome.css';
 
