5dae35090835261e55607623bc54b2be39cc7ffb
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2016-11-11 08:51:50 -0500

    Fixed for zoom out problem in mobile devices

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-//
-// injectTapEventPlugin();
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-// var Main = React.createClass({
-//   render: function() {
-//     return (
-//       <a
-//         href="#"
-//         onTouchTap={this.handleTouchTap}
-//         onClick={this.handleClick}>
-//         Tap Me
-//       </a>
-//     );
-//   },
-//
-//   handleClick: function(e) {
-//     console.log("click", e);
-//   },
-//
-//   handleTouchTap: function(e) {
-//     console.log("touchTap", e);
-//   }
-// });
+injectTapEventPlugin();
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <a
+        href="#"
+        onTouchTap={this.handleTouchTap}
+        onClick={this.handleClick}>
+        Tap Me
+      </a>
+    );
+  },
+
+  handleClick: function(e) {
+    console.log("click", e);
+  },
+
+  handleTouchTap: function(e) {
+    console.log("touchTap", e);
+  }
+});
 
 ReactDOM.render(
   <App />,
