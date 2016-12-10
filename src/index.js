05bf80e6028f62925aaba4a660aa33fcc79c10ba
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2016-12-10 10:41:51 -0500

    removing unused react script for tap tracking

--- src/index.js
+++ src/index.js
@@ -19,31 +19,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-injectTapEventPlugin();
-
-// eslint-disable-next-line
-var Main = React.createClass({
-  render: function() {
-    return (
-      <a
-        href="#"
-        onTouchTap={this.handleTouchTap}
-        onClick={this.handleClick}>
-        Tap Me
-      </a>
-    );
-  },
-
-  handleClick: function(e) {
-    console.log("click", e);
-  },
-
-  handleTouchTap: function(e) {
-    console.log("touchTap", e);
-  }
-});
 
 ReactDOM.render(
   <App />,
