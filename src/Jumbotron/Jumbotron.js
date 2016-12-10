d2e0e32d96cd18388e15974a9e731d9213e4441f
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2016-12-10 10:25:17 -0500

    adding slider functionality and style to be worked on

--- src/Jumbotron/Jumbotron.js
+++ src/Jumbotron/Jumbotron.js
@@ -18,7 +18,7 @@
 
 import React, { Component } from 'react';
 import './jumbotron.css';
-import { viewerResize } from '../Viewer-helpers';
+import { viewerResize, viewerExplode } from '../Viewer-helpers';
 import Viewer from '../Viewer';
 import classnames from 'classnames';
 import scrollTo from 'scroll-to';
@@ -29,11 +29,16 @@ class Jumbotron extends Component {
     super();
 
     this.state = {
-      fullscreen: false
+      fullscreen: false,
+      explode: false,
+      value: 0
     }
 
     this.onFullscreen = this.onFullscreen.bind(this);
+    this.onExplode = this.onExplode.bind(this);
     this.onOrientationChange = this.onOrientationChange.bind(this);
+    this.handleValueChange = this.handleValueChange.bind(this);
+   
   }
 
   componentDidMount() {
@@ -42,6 +47,16 @@ class Jumbotron extends Component {
 
   componentWillUnmount() {
     window.removeEventListener('orientationchange', this.onOrientationChange);
+  }
+
+  handleValueChange(event) {
+    
+    this.setState({
+      value: event.target.value
+    });
+    
+    viewerExplode(this.state.value/100);
+
   }
 
   onFullscreen() {
@@ -55,11 +70,26 @@ class Jumbotron extends Component {
         document.body.classList.add('fullscreen')
       } else {
         document.body.classList.remove('fullscreen');
+        if (this.state.explode){
+          document.body.classList.remove('explode');
+        }
       }
 
       // resize viewer after css animation
       setTimeout(() => viewerResize(), 300);
+      //onScreenShot();
     });
+  }
+
+  onExplode(){
+    this.setState({ explode: !this.state.explode }, () => {
+      if (this.state.explode) {
+        document.body.classList.add('explode')
+      } else {
+        document.body.classList.remove('explode');
+      }
+    });
+    
   }
 
   onOrientationChange() {
@@ -74,6 +104,19 @@ class Jumbotron extends Component {
       'fa-compress': this.state.fullscreen,
     });
 
+
+    const propertiesClass = classnames({
+      'fa': true,
+      'fa-list': this.state.fullscreen,
+    });
+
+    const explodeClass = classnames({
+      'fa': true,
+      'fa-cubes': this.state.fullscreen,
+    });
+
+    
+
     return (
       <div className="forge-jumbotron">
         <Viewer />
@@ -84,6 +127,17 @@ class Jumbotron extends Component {
           <button className="forge-btn" onClick={this.onFullscreen}>
             <i className={buttonClass}></i>
           </button>
+          <button className="explode-btn" onClick={this.onExplode} >
+            <i className={explodeClass}></i>
+          </button>
+          <input type="range" 
+              className= "range-style"
+              min="0" 
+              max="100"
+              value={this.state.value}
+              onChange={this.handleValueChange}
+          />
+          <span className="range-span" id="range">{this.state.value}</span>
         </div>
       </div>
     );
@@ -91,3 +145,9 @@ class Jumbotron extends Component {
 }
 
 export default Jumbotron;
+
+
+
+// <button className="properties-btn" onClick={this.onFullscreen} >
+          //   <i className={propertiesClass}></i>
+          // </button>
