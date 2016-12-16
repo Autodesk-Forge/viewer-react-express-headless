a6cd0e75b5d370ec5c39d117e6fbd5fbe5a5c46f
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2016-12-16 16:58:19 -0500

    animation functionality and clean up of css jumbotron new urn for model

--- src/Jumbotron/Jumbotron.js
+++ src/Jumbotron/Jumbotron.js
@@ -18,7 +18,7 @@
 
 import React, { Component } from 'react';
 import './jumbotron.css';
-import { viewerResize, viewerExplode } from '../Viewer-helpers';
+import { viewerResize, viewerExplode, toggleExplosion, toggleRotation, stopMotion  } from '../Viewer-helpers';
 import Viewer from '../Viewer';
 import classnames from 'classnames';
 import scrollTo from 'scroll-to';
@@ -31,11 +31,18 @@ class Jumbotron extends Component {
     this.state = {
       fullscreen: false,
       explode: false,
+      expMotion: false,
+      rotMotion: false,
+      resetState: false,
+      disabled: false,
       value: 0
     }
 
     this.onFullscreen = this.onFullscreen.bind(this);
     this.onExplode = this.onExplode.bind(this);
+    this.onExplodeAnimation = this.onExplodeAnimation.bind(this);
+    this.onRotateAnimation = this.onRotateAnimation.bind(this);
+    this.onResetState = this.onResetState.bind(this);
     this.onOrientationChange = this.onOrientationChange.bind(this);
     this.handleValueChange = this.handleValueChange.bind(this);
    
@@ -50,11 +57,9 @@ class Jumbotron extends Component {
   }
 
   handleValueChange(event) {
-    
     this.setState({
       value: event.target.value
     });
-    
     viewerExplode(this.state.value/100);
 
   }
@@ -96,6 +101,41 @@ class Jumbotron extends Component {
     setTimeout(() => viewerResize(), 300);
   }
 
+  onExplodeAnimation() {
+    this.setState({ expMotion: !this.state.expMotion, disabled: true }, () => {
+      if (this.state.expMotion) {
+        document.body.classList.add('explode-motion')
+        toggleExplosion();
+      } else {
+        document.body.classList.remove('explode-motion');
+        toggleExplosion();
+        if (this.state.rotMotion === false){
+           this.setState({disabled: false});
+        }
+      }
+    });
+  }
+
+  onRotateAnimation() {
+    this.setState({ rotMotion: !this.state.rotMotion, disabled: true }, () => {
+      if (this.state.rotMotion) {
+        document.body.classList.add('rotate-motion')
+        toggleRotation();
+      } else {
+        document.body.classList.remove('rotate-motion');
+        toggleRotation();
+        if (this.state.expMotion === false){
+           this.setState({disabled: false});
+        }
+       
+      }
+    });
+  }
+
+  onResetState() {
+    stopMotion();
+  }
+
   render() {
 
     const buttonClass = classnames({
@@ -104,18 +144,52 @@ class Jumbotron extends Component {
       'fa-compress': this.state.fullscreen,
     });
 
-
-    const propertiesClass = classnames({
-      'fa': true,
-      'fa-list': this.state.fullscreen,
-    });
+    // const propertiesClass = classnames({
+    //   'fa': true,
+    //   'fa-list': this.state.fullscreen,
+    // });
 
     const explodeClass = classnames({
       'fa': true,
       'fa-cubes': this.state.fullscreen,
     });
 
-    
+    const explodeMotionClass = classnames({
+      'fa': true,
+      'fa-bomb': this.state.fullscreen,
+    });
+
+    const rotateMotionClass = classnames({
+      'fa': true,
+      'fa-repeat': this.state.fullscreen,
+    });
+
+    const resetClass = classnames({
+      'fa': true,
+      'fa-refresh': this.state.fullscreen,
+    });
+
+    const explodeBtnClass = classnames({
+      'explode-btn': true,
+      'btn--active': this.state.explode
+    })
+
+    const explodeMotionBtnClass = classnames({
+      'explode-motion-btn': true,
+      'expbtn--active': this.state.expMotion
+    })
+
+    const rotateMotionBtnClass = classnames({
+      'rotate-motion-btn': true,
+      'rotbtn--active': this.state.rotMotion
+    })
+
+    const resetBtnClass = classnames({
+      'reset-btn': true,
+      'resetbtn--deactive': !this.state.rotMotion
+    })
+
+   
 
     return (
       <div className="forge-jumbotron">
@@ -127,17 +201,25 @@ class Jumbotron extends Component {
           <button className="forge-btn" onClick={this.onFullscreen}>
             <i className={buttonClass}></i>
           </button>
-          <button className="explode-btn" onClick={this.onExplode} >
+          <button className={explodeBtnClass} onClick={this.onExplode} >
             <i className={explodeClass}></i>
           </button>
+          <button className={explodeMotionBtnClass} onClick={this.onExplodeAnimation} >
+            <i className={explodeMotionClass}></i>
+          </button>
+          <button className={rotateMotionBtnClass} onClick={this.onRotateAnimation} >
+            <i className={rotateMotionClass}></i>
+          </button>
+          <button disabled={this.state.disabled} className={resetBtnClass} onClick={this.onResetState} >
+            <i className={resetClass}></i>
+          </button>
           <input type="range" 
-              className= "range-style"
+              className="range-style"
               min="0" 
               max="100"
               value={this.state.value}
               onChange={this.handleValueChange}
           />
-          <span className="range-span" id="range">{this.state.value}</span>
         </div>
       </div>
     );
--- src/Viewer-helpers.js
+++ src/Viewer-helpers.js
@@ -17,10 +17,19 @@
 /////////////////////////////////////////////////////////////////////////////////
 
 
-/* global Autodesk */
+/* global Autodesk, THREE */
 import Client from './Client';
 var viewer;
 var getToken = { accessToken: Client.getaccesstoken()};
+var explodeScale = 0;
+var startExplosion = null;
+var explosionReq;
+var isExploding = false;
+var outwardExplosion = true;
+var startRotation = null;
+var rotationReq;
+var isRotating = false;
+
 
 function launchViewer(div, urn) {
   console.log("Launching Autodesk Viewer for: " + urn);
@@ -107,6 +116,98 @@ export function viewerResize() {
 export function viewerExplode(num){
   viewer.explode(num);
 }
+
+ /**
+     * toggle explosion motion
+     * @param  boolean cancelMotion - true if cancel motion is requested
+     */
+export function toggleExplosion(cancelMotion) {
+      if (cancelMotion || isExploding) {
+        cancelAnimationFrame(explosionReq);
+        isExploding = false;
+        if (cancelMotion) {
+          explodeScale = 0;
+          viewer.explode(explodeScale);
+        }
+      } else {
+        explodeMotion();
+        isExploding = true;
+      }
+    }
+    
+/**
+  * Recursive function for calling requestAnimationFrame for explode motion
+*/
+
+export function explodeMotion(timestamp) {
+      if (!startExplosion) {
+        startExplosion = timestamp;
+      }
+      var progress = timestamp - startExplosion;
+      startExplosion = timestamp;
+      var explodeStep = 0.0002 * (progress || 0);
+      // explode outward and inward
+      if (outwardExplosion) {
+        explodeScale += explodeStep;
+      } else {
+        explodeScale -= explodeStep;
+      }
+      if (explodeScale > 1) {
+        outwardExplosion = false;
+        explodeScale = 1; // this solves when user go to another browser tab
+      } else if (explodeScale < 0) {
+        outwardExplosion = true;
+        explodeScale = 0; // this solves when user go to another browser tab
+      }
+      viewer.explode(explodeScale);
+      explosionReq = window.requestAnimationFrame(explodeMotion);
+    };
+
+ /**
+     * recursive function for rotation motion each time page refreshes
+     */
+export function rotateMotion(timestamp) {
+      if (!startRotation) {
+        startRotation = timestamp;
+      }
+      var progress = timestamp - startRotation;
+      startRotation = timestamp;
+      var rotateStep = 0.0005 * (progress || 0);
+      // get the up axis
+      var worldUp = viewer.navigation.getWorldUpVector();
+      // get the current position
+      var pos = viewer.navigation.getPosition();
+      // copy that position
+      var position = new THREE.Vector3(pos.x, pos.y, pos.z);
+      // set the rotate axis
+      var rAxis = new THREE.Vector3(worldUp.x, worldUp.y, worldUp.z);
+      var matrix = new THREE.Matrix4().makeRotationAxis(rAxis, rotateStep);
+      //apply the new position
+      position.applyMatrix4(matrix);
+      viewer.navigation.setPosition(position);
+      rotationReq = window.requestAnimationFrame(rotateMotion);
+    }
+
+
+     /**
+     * Toggle the rotation movement
+     * @param  boolean cancelMotion true if motion is to be cancelled
+     */
+export function toggleRotation(cancelMotion) {
+      if (cancelMotion || isRotating) {
+        cancelAnimationFrame(rotationReq);
+        isRotating = false;
+      } else {
+        rotateMotion();
+        isRotating = true;
+      }
+    }
+    
+ export function stopMotion() {
+      toggleExplosion(true);
+      toggleRotation(true);
+    }
+
 
 const Helpers = {
   launchViewer,
