b0a7a2f94071862b1636e806c44ff5344d9995f1
Jaime Rosales <jaime.rosales.duque@autodesk.com>
2017-01-05 17:10:10 -0500

    adding toggle functionality for categories display collapse table

--- src/components/Properties/Properties.js
+++ src/components/Properties/Properties.js
@@ -21,51 +21,72 @@ import { connect } from 'react-redux';
 import './properties.css';
 
 class Properties extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      collapsed: new Map()
+    }
+    
+  }
+
+  toggleProperty(propertyIndex, evt) {
+
+    evt.preventDefault();
+    const { collapsed } = this.state;
+    const isCollapsed = collapsed.get(propertyIndex);
+
+    collapsed.set(propertyIndex, !isCollapsed);
+
+    this.setState({ collapsed });
+
+  }
+
+
 	render() {
+    const {collapsed} = this.state;
+
 		return(
-      <div className="model-properties">
+    <div className="model-properties">
         <h3>Properties</h3>
 
-				{!this.props.properties.length
-					? <p><em>Select a part to get started</em></p>
-					: null
-				}
+        {!this.props.properties.length
+          ? <p><em>Select a part to get started</em></p>
+          : null
+        }
 
-        {this.props.properties.map((property, i) => (
-          <ul key={i}>
-            <li>
-              {property.category}
-
-              <ul>
-                {property.data.map((item, itemIndex) => (
-                  <li key={itemIndex}>
-                    <strong>{item.displayName}:</strong> {item.displayValue}
-                  </li>
-                ))}
-              </ul>
-            </li>
-          </ul>
-        ))}
-
-
-        {/*
-          <table className="tg">
-    				<tbody>
-      					<tr>
-        					<th className="tg-yw4l">Category</th>
-      					</tr>
-      					<tr>
-        					<td className="tg-yw4l">one</td>
-      					</tr>
-      					<tr>
-        					<td className="tg-yw4l">two</td>
-      					</tr>
-      					<tr>
-        					<td className="tg-yw4l">tres</td>
-      					</tr>
-      				</tbody>
-    			</table>
-        */}
+        <div className="panel-group">
+          <div className="panel panel-default">
+           
+            {this.props.properties.map((property, i) => (
+             <ul key={i}>
+             <div className="panel-heading">
+                <h4 className="panel-title">
+                  <a data-toggle="collapse" href="#" onClick={this.toggleProperty.bind(this, i)}>{property.category}</a>
+                </h4>
+              </div>     
+                {
+                  collapsed.get(i)
+                    ? (
+                      <div>
+                        <ul className="list-group">
+                            {property.data.map((item, itemIndex) => (
+                              <li key={itemIndex} className="list-group-item">
+                                <strong>{item.displayName}:</strong> {item.displayValue}
+                              </li>
+                            ))}
+                        </ul>
+                      </div>
+                      )
+                    : null
+                }
+             
+              
+            </ul>
+          ))}     
+            
+          </div>
+        </div>
       </div>
 		)
 	}
